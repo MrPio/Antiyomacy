3cfe8c68a17e3c03bf905346886f10af6c5af12f
Valerio Morelli <valeriomorelli50@gmail.com>
2024-01-07 19:55:23 +0100

    Create game.pl

---
+++ game.pl
@@ -0,0 +1,16 @@
+:- use_module(printer).
+:- use_module(map).
+:- use_module(holdings).
+
+play:-
+    generate_random_map(Map),
+    set_owner(Map,0,0,red,Map1),
+    set_owner(Map1,1,0,red,Map2),
+    set_owner(Map2,1,1,red,Map3),
+    set_owner(Map3,1,2,red,Map4),
+    set_owner(Map4,0,2,red,Map5),
+    set_owner(Map5,2,2,blue,Map6),
+    set_owner(Map6,2,1,blue,Map7),
+    find_provinces(Map7,[P1, P2]),
+    province_boundary(Map7,P2,B),
+    write(B).
