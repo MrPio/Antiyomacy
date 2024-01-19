a6e244bef4f6bba8a4ee3280d81db080b57fd137
mrpio <valeriomorelli50@gmail.com>
2024-01-20 00:04:33 +0100

    TODO updated

--- game.pl
+++ game.pl
@@ -1,8 +1,6 @@
 :- use_module([printer, map, province, unit, building, economy]).
 
 /* TODO:
-    • The farm cost should increase with the number of already bought farms,
-      so a predicate to count the number of farms in a given province is needed. (Federica)
     • At the beginning of the game, the two provinces are randomly generated and located far apart.
     • Multiple purchase actions if enough money.
     • Test units attack.
@@ -11,13 +9,15 @@
     • Test province split due to enemy attack. Money should split based on provinces size.
     • Two units of the same level may join together to form a stronger unit.
     • Trees cannot randomly spawn during the gameplay. Instead they can spawn at the beginning
-      of the game, or supply centers can be introduced.
+    of the game, or supply centers can be introduced.
     -------------------------------------------------------------------------------------------------
     X When applying a move, should I update both the Map and the Province or just the former?
     X The newly placed unit should not move before the next turn; that is because it can be
-      placed directly outside the province boundary already at purchase time.
+    placed directly outside the province boundary already at purchase time.
     X Farm should be placed inside the province and near other farms. The first farm can be
-      placed anywhere inside the province
+    placed anywhere inside the province
+    X The farm cost should increase with the number of already bought farms,
+      so a predicate to count the number of farms in a given province is needed. (Federica)
  */
 
 /* Things to write in the paper:
