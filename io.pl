56c01612ff0b7a8d2173afd614edb5c77221287a
fedeStaffo <fede01staf@gmail.com>
2024-02-04 19:07:35 +0100

    Added check invalid move and small fixes in i/o

--- io.pl
+++ io.pl
@@ -1,4 +1,4 @@
-:- module(io, [ask_game_mode/1, player_move/1, purchase_input/2, displace_input/2]).
+:- module(io, [ask_game_mode/1, ask_color/1, player_move/1, purchase_input/2, displace_input/2]).
 :- use_module([utils, map, hex, province, unit, building, economy, eval, minimax]).
 :- use_module(library(random)).
 :- use_module(library(readutil)).
@@ -31,7 +31,6 @@ ask_color(Color):-
     ;   writeln('Invalid choice'),
         ask_color(Color)
     ).
-    % The caller: format('You play as ~w player', Color)
 
 % player_move(-Choice)
 player_move(Choice):-
@@ -40,11 +39,11 @@ player_move(Choice):-
     writeln('2) Purchase'),
     writeln('3) Skip turn'),
     read_line_to_string(user_input, Choice1),
-    (Choice1 == 1 ->
+    (Choice1 == "1" ->
         Choice = Choice1;
-    Choice1 == 2 ->
+    Choice1 == "2" ->
         Choice = Choice1;
-    Choice1 == 3 ->
+    Choice1 == "3" ->
         Choice = Choice1;
     writeln('Invalid choice'),
     player_move(Choice)
@@ -72,36 +71,31 @@ displace_input([X1,Y1], [X2,Y2]):-
 
 % Predicate to handle user input for the "Purchase" move
 % purchase_input(-ResName, -[X,Y])
-purchase_input(ResName,[X,Y]):-
+purchase_input(ResName, [X, Y]):-
     writeln('ResName (options: farm, tower, strong_tower, peasant, spearman, baron, knight):'),
     read_line_to_string(user_input, ResName1),
     writeln('ToCoord (format X-Y):'),
     read_line_to_string(user_input, ToCoord),
-    (validate_resource_name(ResName1), validate_coordinate(ToCoord,[X,Y]) ->
-        ResName = ResName1
+    (validate_resource_name(ResName1, ResNameAtom), validate_coordinate(ToCoord, [X, Y]) ->
+        ResName = ResNameAtom
     ; 
         writeln('Invalid input. Please try again.'),
-        purchase_input(ResName,[X,Y])
+        purchase_input(ResName, [X, Y])
     ).
 
 % Validate coordinates and return the format [X,Y]
-% validate_coordinate(+Coord,-[X,Y])
-validate_coordinate(Coord,[X,Y]):-
-    % Convert the Prolog term to an atom
-    term_to_atom(Coord, AtomCoord),
-    % Convert the atom to a list of character codes
-    atom_codes(AtomCoord, Codes),
-    % Split the codes using "-" as a delimiter, obtaining XStr and YStr
-    split_string(Codes, "-", "", [XStr, YStr]),
-    % Convert XStr and YStr to integers X and Y
-    number_codes(X, XStr),
-    number_codes(Y, YStr),
-    % Check that both X and Y are greater than 0 and inside map
+% validate_coordinate(+Coord, -[X,Y])
+validate_coordinate(Coord, [X, Y]) :-
+    split_string(Coord, "-", "", [XStr, YStr]),
+    atom_number(XStr, X),
+    atom_number(YStr, Y),
     X >= 0,
     Y >= 0,
     inside_map([X, Y]).
 
-% Validate the resource name
-% validate_resource_name(+ResName)
-validate_resource_name(ResName):-
-    member(ResName, [farm, tower, strong_tower, peasant, spearman, baron, knight]).
+
+% Validate the resource name and convert to atom
+% validate_resource_name(+ResName, -AtomResName)
+validate_resource_name(ResName, AtomResName):-
+    member(ResName, ["farm", "tower", "strong_tower", "peasant", "spearman", "baron", "knight"]),
+    atom_string(AtomResName, ResName).
