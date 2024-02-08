3e2a3333707ccf4255bf0c178567e9e921c99ce6
mrpio <valeriomorelli50@gmail.com>
2024-02-08 17:12:54 +0100

    Units and building image fix. Hex selection fixed

--- gui.pl
+++ gui.pl
@@ -1,11 +1,12 @@
-:- module(gui, [gui/1]).
+:- module(gui, [gui/0]).
 :- use_module([map, hex, province, utils]).
 :- use_module(library(pce)).
-:- dynamic selected_unit/4.
+:- dynamic([selected_hex/1, selected_unit/4]).
+
 
 test_gui:-
     % map generation:
-    generate_random_map(_, false),
+    generate_random_map(_, true),
     RedHexes=[[0,0],[1,0],[1,1],[1,2],[0,2]],
     BlueHexes=[[2,2],[3,2],[3,3],[3,4],[4,3]],
     Buildings=[[2,2]-farm],
@@ -16,27 +17,32 @@ test_gui:-
     foreach(member(Coord-Unit,Units),set_unit(Coord,Unit)),
     map(Map),
     print_map(Map),
-
-    new(@window, picture('Antiyomacy')),
-    gui(Map).
-
-gui(Map):-
-    send(@window, clear),
+    init_gui,
     
+    % send(@window, kind, popup),
+    gui.
+
+init_gui:-
+    free(@window),
+    new(@window, picture('Antiyomacy')).
+
+gui:-
+    map(Map),
+    send(@window, clear),
     % resources:
-    new(PeasantImage, image('resources/sprites/unit/peasant.gif')),
-    new(SpearmanImage, image('resources/sprites/unit/spearman.gif')),
-    new(BaronImage, image('resources/sprites/unit/baron.gif')),
-    new(KnightImage, image('resources/sprites/unit/knight.gif')),
-    new(CastleImage, image('resources/sprites/building/castle1.gif')),
-    new(FarmImage, image('resources/sprites/building/farm1.gif')),
-    new(TowerImage, image('resources/sprites/building/tower1.gif')),
-    new(StrongTowerImage, image('resources/sprites/building/strong_tower1.gif')),
-    new(SeaImage, image('resources/sprites/sea/idle_dark_small_2.gif')),
-    new(FreeImage, image('resources/sprites/grass/grass.gif')),
-    new(RedImage, image('resources/sprites/grass/grass_red.gif')),
-    new(BlueImage, image('resources/sprites/grass/grass_blue.gif')),
-    new(YellowImage, image('resources/sprites/grass/grass_yellow.gif')),
+    (   new(PeasantImage, image('resources/sprites/unit/peasant.gif')),
+        new(SpearmanImage, image('resources/sprites/unit/spearman.gif')),
+        new(BaronImage, image('resources/sprites/unit/baron.gif')),
+        new(KnightImage, image('resources/sprites/unit/knight.gif')),
+        new(FarmImage, image('resources/sprites/building/farm.gif')),
+        new(TowerImage, image('resources/sprites/building/tower.gif')),
+        new(StrongTowerImage, image('resources/sprites/building/strong_tower.gif')),
+        new(SeaImage, image('resources/sprites/sea/idle_dark_small_2.gif')),
+        new(FreeImage, image('resources/sprites/grass/grass.gif')),
+        new(RedImage, image('resources/sprites/grass/grass_red.gif')),
+        new(BlueImage, image('resources/sprites/grass/grass_blue.gif')),
+        new(YellowImage, image('resources/sprites/grass/grass_yellow.gif'))
+    ),
 
     % parameters:
     MapSize = 7,
@@ -44,69 +50,66 @@ gui(Map):-
     TotalSize = MapSize* HexSize + HexSize,
 
     send(@window, size, size(TotalSize, TotalSize)),
-    (   between(0, MapSize, X),
-        between(0, MapSize, Y),
-        get_hex(Map, [X, Y], hex(_, _, Tile, Owner, Building, Unit)),
-        GX is X * HexSize,
-        GY is Y * HexSize,
-        (   sea(Tile)
-        ->  % Draw sea tile
-            get(SeaImage, size, size(ImgWidth, ImgHeight)),
-            CenterX is GX + (HexSize - ImgWidth) / 2,
-            CenterY is GY + (HexSize - ImgHeight) / 2,
-            send(@window, display, bitmap(SeaImage), point(CenterY, CenterX))
-        ;   % Draw grass tile
-
-            % Todo, use the following to draw the grass tiles, instead of box
-            nth0(PlayerIndex, [red, blue, none], Owner), nth0(PlayerIndex, [RedImage, BlueImage, FreeImage], Image),
-            get(Image, size, size(ImgWidth, ImgHeight)),
-            CenterX is GX + (HexSize - ImgWidth) / 2,
-            CenterY is GY + (HexSize - ImgHeight) / 2,
-            new(GrassBitmap, bitmap(Image)),
-            send(GrassBitmap, recogniser, click_gesture(left, '', single, message(@prolog, on_box_select, GrassBitmap))),
-            send(@window, display, GrassBitmap, point(CenterY, CenterX))
-
-            % nth0(PlayerIndex, [red, blue, none], Owner), nth0(PlayerIndex, [red, blue, grey], Color),
-            % new(M, box(HexSize,HexSize)),
-            % send(M, fill_pattern, colour(Color)),
-            % send(M, recogniser, click_gesture(left, '', single, message(@prolog, on_box_select, M))),
-            % send(@window, display, M, point(GY,GX))
-        ), 
-
-        % Draw the unit
-        % send(@window, display, bitmap(Image),point(CenterX,CenterY)),
-        (   Unit \= none -> 
-            (   Unit = peasant -> Image = PeasantImage
-            ;   Unit = spearman -> Image = SpearmanImage
-            ;   Unit = baron -> Image = BaronImage
-            ;   Unit = knight -> Image = KnightImage
-            ),
-            get(Image, size, size(ImgWidth, ImgHeight)),
-            CenterX is GX + (HexSize - ImgWidth) / 2,
-            CenterY is GY + (HexSize - ImgHeight) / 2,
-            send(@window, display, bitmap(Image), point(CenterY, CenterX))
+    (   % Check if there is any selected hex
+        (   retract(selected_hex(SelectedHex))
+        ->  hex_coord(SelectedHex, SelectedCoord)
         ;   true
         ),
+        between(0, MapSize, X),
+        between(0, MapSize, Y),
+        get_hex(Map, [X, Y], Hex),
+        hex_tile(Hex, Tile), % Get
+        hex_owner(Hex, Owner), % Get
+        hex_unit(Hex, Unit), % Get
+        hex_building(Hex, Building), % Get
+        GX is X * HexSize,
+        GY is Y * HexSize,
+        
+        % Draw the tile
+        (   sea(Tile)
+        ->  TileImage=SeaImage
+        ;   SelectedCoord == [X,Y]
+        ->  TileImage=YellowImage
+        ;   nth0(PlayerIndex, [red, blue, none], Owner), 
+            nth0(PlayerIndex, [RedImage, BlueImage, FreeImage], TileImage)
+        ), 
+        get(TileImage, size, size(TileWidth, TileHeight)),
+        TileX is GX + (HexSize - TileWidth) / 2,
+        TileY is GY + (HexSize - TileHeight) / 2,
+        new(GrassBitmap, bitmap(TileImage)),
+        send(GrassBitmap, recogniser, click_gesture(left, '', single, message(@prolog, on_hex_select, X, Y))),
+        send(@window, display, GrassBitmap, point(TileY, TileX)),
+        % Draw the unit if present
+        (   Unit == none
+        ->  true
+        ;   nth0(UnitIndex, [peasant, spearman, baron, knight], Unit), 
+            nth0(UnitIndex, [PeasantImage, SpearmanImage, BaronImage, KnightImage], UnitImage),
+            get(UnitImage, size, size(UnitWidth, UnitHeight)),
+            UnitX is GX + (HexSize - UnitWidth) / 2,
+            UnitY is GY + (HexSize - UnitHeight) / 2,
+            send(@window, display, bitmap(UnitImage), point(UnitY, UnitX))
+        ),
 
-        % Draw the building
-        (   Building \= none ->
-            (   Building = castle -> Image = CastleImage
-            ;   Building = farm -> Image = FarmImage
-            ;   Building = tower -> Image = TowerImage
-            ;   Building = strong_tower -> Image = StrongTowerImage
-            ),
-            get(Image, size, size(ImgWidth, ImgHeight)),
-            CenterX is GX + (HexSize - ImgWidth) / 2,
-            CenterY is GY + (HexSize - ImgHeight) / 2,
-            send(@window, display, bitmap(Image), point(CenterY, CenterX ))
-        ;   true
+        % Draw the building if present
+        (   Building == none
+        ->  true
+        ;   nth0(BuildingIndex, [farm, tower, strong_tower], Building), 
+            nth0(BuildingIndex, [FarmImage, TowerImage, StrongTowerImage], BuildingImage),
+            get(BuildingImage, size, size(BuildingWidth, BuildingHeight)),
+            BuildingX is GX + (HexSize - BuildingWidth) / 2,
+            BuildingY is GY + (HexSize - BuildingHeight) / 2,
+            send(@window, display, bitmap(BuildingImage), point(BuildingY, BuildingX))
         ),
 
         fail ; true
     ),
-    send(@window, open),
-    true.
+    send(@window, open).
     
+on_hex_select(X,Y) :-
+    map(Map),
+    get_hex(Map, [X,Y], Hex),
+    assert(selected_hex(Hex)),
+    gui.
 
 on_box_select(Box) :-
     % Get the position of the box
